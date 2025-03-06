8222cab61270fadaa0ad5c05bab11ea87f0ec651
vivekb20496 <vivekbrahmbhatt2096@gmail.com>
2025-03-06 19:25:14 +0530

    Update and rename app.py to home.py

---
+++ app/main_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import re
+import plotly
+import plotly.graph_objects as go
+from plotly._subplots import make_subplots
+import plotly.express as px
+
+
+df = pd.read_csv('The-Office-Lines-V4.csv')
+
+df = df.drop(columns=['Unnamed: 6'])
+
+df = df.drop_duplicates(subset=['season', 'episode', 'title', 'scene', 'speaker', 'line'])
+
+df.reset_index(drop=True, inplace=True)
+
+def extract_unique_names(speaker):
+    names = set(re.split(r'\s*(?:,|&|\band\b|/)\s*', speaker)) 
+    names.discard('') 
+    return ', '.join(sorted(names))
+
+df['unique_speakers'] = df['speaker'].apply(extract_unique_names)
+
+df['unique_speakers'] = df['unique_speakers'].str.strip().str.replace(r'^[:"]+|[:"]+$', '', regex=True)
+
+df["unique_speakers"] = df["unique_speakers"].str.split(", ")
+
+
+df_exploded = df.explode("unique_speakers", ignore_index=True)
+
+
+
+)
+
+st.plotly_chart(fig)
