afa361224fb4695387f0e065406e56b00276d35f
vivekb20496 <vivekbrahmbhatt2096@gmail.com>
2025-03-06 22:37:51 +0530

    Create Q6.py

---
+++ pages/Q6.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+from home import df_exploded
+
+st.title("Q6. That's What She Said Analysis")
+
+filtered_df = df_exploded[df_exploded["line"].str.contains(r"\bThat's what she said\b", case=False, na=False, regex=True)]
+
+filtered_df_q6 = pd.DataFrame(filtered_df, columns=['season', 'episode', 'title', 'scene', 'unique_speakers', 'line'])
+
+filtered_df_q6 = filtered_df_q6.rename(columns={'season': 'Season', 'episode': 'Episode', 'title':'Title', 'scene': 'Scene', 'unique_speakers':'Character', 'line':'Line'})
+st.dataframe(filtered_df_q6)
